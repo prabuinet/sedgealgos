42b44c31f10fb5aa7832fe2a8e1867747e28bb1a
Prabu Natarajan <prabuinet@gmail.com>
2021-02-04 15:46:09 +0530

    min span

---
+++ src/course/IndexMinPQ.java
@@ -0,0 +1,38 @@
+package course;
+
+public class IndexMinPQ <Key extends Comparable<Key>> {
+
+	IndexMinPQ(int N) {
+		
+	}
+	
+	void insert(int i , Key key) {
+		
+	}
+	
+	void decreaseKey(int i, Key key) {
+		
+	}
+	
+//	boolean contains(int i) {
+//		
+//	}
+//	
+//	int delMin() {
+//		
+//	}
+//	
+//	boolean isEmpty() {
+//		
+//	}
+//	
+//	int size() {
+//		
+//	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/course/MinimumSpanning.java
+++ src/course/MinimumSpanning.java
@@ -40,14 +40,53 @@ public class MinimumSpanning {
 	}
 	
 	
-	private static class Prims {
+	private static class LazyPrims {
 		// start with vertex 0 and greedily grow tree T
 		// add to T the min weight edge with exactly one endpoint in T
 		// repeat until V-1 edges
 		
+		private boolean[] marked;
+		private Queue<Edge> mst;
+		private MinPQ<Edge> pq;
 		
+		public LazyPrims(EdgeWeightedGraph G) {
+			pq = new MinPQ<Edge>();
+			mst = new Queue<Edge>();
+			marked = new boolean[G.V()];
+			
+			visit(G, 0);
+			
+			while(!pq.isEmpty()) {
+				Edge e = pq.delMin();
+				int v = e.either(), w = e.other(v);
+				if(marked[v] && marked[w]) continue;
+				mst.enqueue(e);
+				if(!marked[v]) visit(G, v);
+				if(!marked[w]) visit(G, w);
+			}
+			
+		}
+		
+		private void visit(EdgeWeightedGraph G, int v) {
+			marked[v] = true;
+			for(Edge e: G.adj(v)) {
+				if(!marked[e.other(v)])
+					pq.insert(e);
+			}
+		}
+		
+		public Iterable<Edge> mst() {
+			return mst;
+		}
 	}
 	
+	
+	public static class EagerPrim {
+		
+		public EagerPrim(EdgeWeightedGraph G) {
+			
+		}
+	}
 	
 	public static void main(String[] args) {
 		
